6c3154c7f46606a275b89952deed7e0dea7c581e
Ming-Hung Tsai <mingnus@gmail.com>
2021-07-11 15:22:56 +0800

    Trying to solve question 4

--- q4/queue.c
+++ q4/queue.c
@@ -102,7 +102,7 @@ int con_push(con_queue_t *restrict queue, void *restrict new_element)
 
     /* Add to queue with lock */
     mtx_lock(queue->last_mutex);
-    AAA;
+    queue->last->next = node;
     queue->last = node;
     mtx_unlock(queue->last_mutex);
 
@@ -127,8 +127,8 @@ void *con_pop(con_queue_t *queue)
     }
 
     /* Queue not empty: retrieve data and rewire */
-    void *return_value = BBB;
-    CCC;
+    void *return_value = node->value;
+    queue->first = new_header;
 
     mtx_unlock(queue->first_mutex);
 
